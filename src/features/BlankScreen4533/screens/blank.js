64c8e2c63f04ec4ae6203235024415d1d05ac72a
Crowdbotics <team@crowdbotics.com>
2020-05-23 00:04:27 +0000

    Update blank.js JSX

--- src/features/BlankScreen4533/screens/blank.js
+++ src/features/BlankScreen4533/screens/blank.js
@@ -1,17 +1,107 @@
-import React from "react";
-import { View } from "react-native";
-import { withStyles, Text, Button, CheckBox, Radio, Toggle, Icon, Input } from 'react-native-ui-kitten';
+import React from "react"
+import { View } from "react-native"
+import {
+  withStyles,
+  Text,
+  Button,
+  CheckBox,
+  Radio,
+  Toggle,
+  Icon,
+  Input
+} from "react-native-ui-kitten"
 
 export class _Blank extends React.Component {
   static navigationOptions = {
     title: "Blank".toUpperCase()
-  };
+  }
 
-  render = () => <View style={this.props.themedStyle.container}></View>;
+  render = () => (
+    <View
+      style={{
+        overflow: "visible",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        alignContent: "stretch",
+        borderStyle: "solid",
+        backgroundSize: "auto"
+      }}
+    >
+      <View
+        style={{
+          overflow: "visible",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "flex-start",
+          alignItems: "stretch",
+          alignContent: "stretch",
+          borderStyle: "solid",
+          backgroundSize: "auto"
+        }}
+      >
+        <View
+          style={{
+            overflow: "visible",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+            alignItems: "stretch",
+            alignContent: "stretch",
+            borderStyle: "solid",
+            backgroundSize: "auto"
+          }}
+        >
+          <Text
+            style={{
+              overflow: "visible",
+              color: "#f830b9",
+              fontWeight: "bold",
+              fontFamily: "Palatino Linotype",
+              borderStyle: "solid"
+            }}
+          >
+            Hello!
+          </Text>
+          <Slider
+            value="50"
+            minimalValue="0"
+            maximumValue="100"
+            step="1"
+            disabled={false}
+            style={{ overflow: "visible", borderStyle: "solid" }}
+          />
+        </View>
+        <View
+          style={{
+            overflow: "visible",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+            alignItems: "stretch",
+            alignContent: "stretch",
+            borderStyle: "solid",
+            backgroundSize: "auto"
+          }}
+        >
+          <Button
+            style={{
+              overflow: "visible",
+              borderStyle: "solid",
+              backgroundColor: "#f92fdb"
+            }}
+          >
+            Press me
+          </Button>
+        </View>
+      </View>
+    </View>
+  )
 }
 
 export default Blank = withStyles(_Blank, theme => ({
   container: {
     backgroundColor: theme["color-basic-100"]
   }
-}));
+}))
